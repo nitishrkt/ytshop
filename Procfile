c8b00eb0f9484bce722b020495386e03dd99fce5
Nitish Sharma <nitishrkt@gmail.com>
2020-04-22 14:42:51 +0530

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ytshop.wsgi --log-file -
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YTShop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ytshop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
